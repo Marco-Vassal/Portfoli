26028fb08cb19ce4bcb97ec3842e1506efb8d46d
Marco-Vassal <marco.vassal08@gmail.com>
2023-05-22 20:29:48 +0200

    welcome page static 90%

--- script/app.js
+++ script/app.js
@@ -6,16 +6,28 @@ const expand = (item, i) => {
   items.forEach((it, ind) => {
     if (i === ind) return
     it.clicked = false
+    it.getElementsByClassName('div_shown')[0].style.display = "none"
+    it.getElementsByClassName('div_hidden')[0].style.display = "flex"
   })
   gsap.to(items, {
     width: item.clicked ? '10vw' : '10vw',
+    height: item.clicked ? '24vw' : '24vw',
     duration: 2,
     ease: 'elastic(1, .6)'
   })
-  
+  if (item.clicked) {
+    console.log(item.getElementsByClassName('div_shown'))
+    item.getElementsByClassName('div_shown')[0].style.display = "none"
+    item.getElementsByClassName('div_hidden')[0].style.display = "flex"
+  } else {
+    item.getElementsByClassName('div_shown')[0].style.display = "block"
+    item.getElementsByClassName('div_hidden')[0].style.display = "none"
+  }
+
   item.clicked = !item.clicked
   gsap.to(item, {
     width: item.clicked ? '15vw' : '10vw',
+    height: item.clicked ? '30vw' : '24vw',
     duration: 2.5,
     ease: 'elastic(1, .3)'
   })
