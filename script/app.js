06097206196c9d395cd50b317fc9633b8c4ee902
Marco-Vassal <marco.vassal08@gmail.com>
2023-06-06 12:56:04 +0200

    responsive 412px

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 const items = document.querySelectorAll('.item')
 const cercle = document.querySelector('.cercle')
-const header = document.getElementById('myHeader')
+const header = document.querySelector('header')
 const loader = document.querySelector('.loader')
 const main = document.querySelector('main')
 const pages = document.querySelectorAll('.navbar a')
@@ -8,43 +8,86 @@ const sticky = header.offsetTop // Get the offset position of the navbar
 const ratio = .1
 
 // Elastic accordion for quality
-const expand = (item, i) => {
-  items.forEach((it, ind) => {
-    if (i === ind) return
-    it.clicked = false
-    it.getElementsByClassName('div_shown')[0].style.display = "none"
-    it.getElementsByClassName('div_hidden')[0].style.display = "flex"
-  })
-  gsap.to(items, {
-    width: item.clicked ? '10vw' : '10vw',
-    height: item.clicked ? '24vw' : '24vw',
-    duration: 2,
-    ease: 'elastic(1, .6)'
-  })
-  if (item.clicked) {
-    console.log(item.getElementsByClassName('div_shown'))
-    item.getElementsByClassName('div_shown')[0].style.display = "none"
-    item.getElementsByClassName('div_hidden')[0].style.display = "flex"
-  } else {
-    item.getElementsByClassName('div_shown')[0].style.display = "block"
-    item.getElementsByClassName('div_hidden')[0].style.display = "none"
+if (window.matchMedia("(min-width: 413px)").matches) {
+  /* La largeur minimum de l'affichage est 391 px inclus */
+  const expand = (item, i) => {
+    items.forEach((it, ind) => {
+      if (i === ind) return
+      it.clicked = false
+      it.getElementsByClassName('div_shown')[0].style.display = "none"
+      it.getElementsByClassName('div_hidden')[0].style.display = "flex"
+    })
+    gsap.to(items, {
+      width: item.clicked ? '10vw' : '10vw',
+      height: item.clicked ? '24vw' : '24vw',
+      duration: 2,
+      ease: 'elastic(1, .6)'
+    })
+    if (item.clicked) {
+      console.log(item.getElementsByClassName('div_shown'))
+      item.getElementsByClassName('div_shown')[0].style.display = "none"
+      item.getElementsByClassName('div_hidden')[0].style.display = "flex"
+    } else {
+      item.getElementsByClassName('div_shown')[0].style.display = "block"
+      item.getElementsByClassName('div_hidden')[0].style.display = "none"
+    }
+  
+    item.clicked = !item.clicked
+    gsap.to(item, {
+      width: item.clicked ? '15vw' : '10vw',
+      height: item.clicked ? '30vw' : '24vw',
+      duration: 2.5,
+      ease: 'elastic(1, .3)'
+    })
   }
-
-  item.clicked = !item.clicked
-  gsap.to(item, {
-    width: item.clicked ? '15vw' : '10vw',
-    height: item.clicked ? '30vw' : '24vw',
-    duration: 2.5,
-    ease: 'elastic(1, .3)'
+  items.forEach((item, i) => {
+    item.clicked = false
+    if(i === 2) {
+      item.clicked = true
+    }
+    item.addEventListener('click', () => expand(item, i))
+  })
+} else {
+  /* L'affichage est inférieur à 391px de large */
+  const expand = (item, i) => {
+    items.forEach((it, ind) => {
+      if (i === ind) return
+      it.clicked = false
+      it.getElementsByClassName('div_shown')[0].style.display = "none"
+      it.getElementsByClassName('div_hidden')[0].style.display = "flex"
+    })
+    gsap.to(items, {
+      width: item.clicked ? '75vw' : '75vw',
+      height: item.clicked ? '24vw' : '24vw',
+      duration: 2,
+      ease: 'elastic(1, .6)'
+    })
+    if (item.clicked) {
+      console.log(item.getElementsByClassName('div_shown'))
+      item.getElementsByClassName('div_shown')[0].style.display = "none"
+      item.getElementsByClassName('div_hidden')[0].style.display = "flex"
+    } else {
+      item.getElementsByClassName('div_shown')[0].style.display = "block"
+      item.getElementsByClassName('div_hidden')[0].style.display = "none"
+    }
+  
+    item.clicked = !item.clicked
+    gsap.to(item, {
+      width: item.clicked ? '75vw' : '75vw',
+      height: item.clicked ? '90vw' : '24vw',
+      duration: 3,
+      ease: 'elastic(0.5, .3)'
+    })
+  }
+  items.forEach((item, i) => {
+    item.clicked = false
+    if(i === 0) {
+      item.clicked = true
+    }
+    item.addEventListener('click', () => expand(item, i))
   })
 }
-items.forEach((item, i) => {
-  item.clicked = false
-  if(i === 2) {
-    item.clicked = true
-  }
-  item.addEventListener('click', () => expand(item, i))
-})
+
 
 // animation when scrolling
 const options = {
@@ -77,7 +120,7 @@ window.addEventListener('load', ()=> {
     }, 400)
   }
   const cercle = document.querySelector('.cercle')
-  const height = document.body.offsetHeight/2
+  const height = document.body.clientHeight/2
   cercle.style.height = height + "px"
 })
 
@@ -94,8 +137,26 @@ window.addEventListener("scroll", function() {
 })
 
 window.addEventListener("resize", () => {
-
   const cercle = document.querySelector('.cercle')
-  const height = document.body.offsetHeight/2
+  const height = document.body.clientHeight/2
+  console.log(height)
   cercle.style.height = height + "px"
 });
+
+
+//header hamburger
+const hamburger = document.querySelector(".hambergur")
+console.log(hamburger)
+hamburger.addEventListener("click", () => {
+    
+    document.querySelector(".fullscreen").classList.toggle("active")
+  });
+  
+  document.querySelector(".close").addEventListener("click", () => {
+    document.querySelector(".fullscreen").classList.toggle("reverse_anim")
+    setTimeout(()=>{
+        document.querySelector(".fullscreen").classList.remove("reverse_anim")
+        document.querySelector(".fullscreen").classList.remove("active")
+    }, 400)
+  });
+  
--- script/header_footer.js
+++ script/header_footer.js
@@ -1,7 +1,7 @@
 class Header extends HTMLElement {
     connectedCallback() {
-      this.innerHTML = `
-        <header id="myHeader">
+        this.innerHTML = `
+        <header class="header-desktop">
             <nav class="navbar">
                 <a href="index.html">Marco Vassal</a>
                 <ul>
@@ -11,16 +11,35 @@ class Header extends HTMLElement {
                 </ul>
             </nav>
             <div class="ligne_head"></div>
+        </header>
+        <header class="header-ham">
+            <div class="logo"><a href="index.html">Marco Vassal</a></div>
+            <div class="hambergur">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <nav class="navbar fullscreen">
+            <ul>
+                <li style="--animation-order: 1;"><a href="index.html">Accueil</a></li>
+                <li style="--animation-order: 2;"><a href="index.html#competences">Compétences</a></li>
+                <li style="--animation-order: 3;"><a href="services.html">Services</a></li>
+                <li style="--animation-order: 4;"><a href="contact.html">Contact</a></li>
+            </ul>
+            <div class="close">
+                <div></div>
+            </div>
+            </nav>
         </header>`
     }
-  }
-  
-  //Footer
-  
-  class Footer extends HTMLElement {
+}
+
+//Footer
+
+class Footer extends HTMLElement {
     connectedCallback() {
-      this.innerHTML = 
-        `<footer class="reveal">
+        this.innerHTML =
+            `<footer class="reveal footer_desktop">
             <div class="bar reveal-1"></div>
             <div class="footer_ctn">
                 <div class="footer_info">
@@ -50,10 +69,39 @@ class Header extends HTMLElement {
                     <a class="reveal-4" target="_blank" href="https://www.instagram.com/marco_vassal/">Instagram</a>
                 </div>
             </div>
-        </footer>`
-      
+        </footer>
+        <footer class="reveal footer_phone">
+            <div class="bar reveal-1"></div>
+                <div class="footer_ctn">
+                    <div class="footer_res">
+                        <a class="reveal-1" target="_blank" href="https://www.linkedin.com/in/marco-vassal-144327204"><img src="images/Logo/LinkedIn_logo_footer.svg" alt=""></a>
+                        <a class="reveal-2" target="_blank" href="https://www.malt.fr/profile/marcovassal"><img src="images/Logo/Malt_logo_footer.svg" alt=""></a>
+                        <a class="reveal-3" target="_blank" href="https://www.instagram.com/marco_vassal/"><img src="images/Logo/Instagram_logo.svg" alt=""></a>
+                    </div>
+                    <div class="footer_info">
+                        <p class="reveal-1">Site codé par MARCO VASSAL</p>
+                        <p class="reveal-2">Developpeur web front end freelance</p>
+                        <a class="reveal-3" href="tel:+33644870471">TEL: 06 44 87 04 71</a>
+                        <a class="reveal-4" href="mailto:marco.vassal07@gmail.com">EMAIL: Marco.vassal07@gmail.com</a>   
+                    </div>
+                    <div class="footer_nav">
+                        <h2 class="reveal-1">Navigation</h2>
+                        <a class="reveal-2" href="index.html">Accueil</a>
+                        <a class="reveal-3" href="index.html#competences">Compétences</a>
+                        <a class="reveal-4" href="serices.html">Services</a>
+                        <a class="reveal-5" href="contact.html">Contact</a>
+                    </div>
+                </div>
+                <div class="info_stack reveal-5">
+                <p>Stack utilisé: HTML/CSS/JS(Gsap)</p>
+                <p>© Copyright 2023 - Marco VASSAL. Tous droits réservés</p>
+                <a href="mentions-legales.html">Mentions légales</a>
+            </div>
+        </footer>
+        `
+
     }
-  }
-  
-  customElements.define('main-header', Header);
-  customElements.define('main-footer', Footer);
+}
+
+customElements.define('main-header', Header);
+customElements.define('main-footer', Footer);
